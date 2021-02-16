f42efcf69f7167783fdd4c3f80b236f65f615947
hhimanshu <harit.himanshu@yahoo.in>
2021-02-16 08:30:40 -0800

    Added support for custom font

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
     entry: './src/index.js',
+    mode: "production",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
@@ -12,6 +13,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.ttf$/i,
+                type: "asset/resource"
+            }
         ],
     },
 }
