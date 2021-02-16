d44d6d833a9f57418f101055f333ba6a9b2cdbf1
hhimanshu <harit.himanshu@yahoo.in>
2021-02-16 08:45:41 -0800

    Create dist folder every run using html-webpack-plugin and clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     mode: "production",
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
     module: {
