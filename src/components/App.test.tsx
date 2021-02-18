337c0107cfa6af60b86bd04db73590589a7ab3f4
hhimanshu <harit.himanshu@yahoo.in>
2021-02-18 06:54:19 -0800

    Added test for React Component, needed to added babel preset for react. Also added jest configuration using command-line "jest --init"

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,5 +1,17 @@
+import {render, screen} from "@testing-library/react";
+import React from "react";
+import App from "./App";
+
 describe("A test", () => {
     it('2 + 2 = 4', () => {
         expect(2 + 2).toEqual(4)
     });
-})
+});
+
+describe("Test App Component", () => {
+    it('should display h1', () => {
+        render(<App/>)
+        const h1 = screen.getByRole('heading')
+        expect(h1.innerHTML).toEqual("Hello React in TypeScript + Jest + Enzyme")
+    });
+});
