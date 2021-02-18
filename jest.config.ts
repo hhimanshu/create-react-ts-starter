c565f8609bab3c63cb8a370387d5bfcd33a83139
hhimanshu <harit.himanshu@yahoo.in>
2021-02-18 08:59:57 -0800

    Collect Code coverage and use babel-eslint

--- jest.config.ts
+++ jest.config.ts
@@ -21,27 +21,21 @@ export default {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    "src/**/*.{js, jsx, ts, tsx}"
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!<rootDir>/node_modules/"
   ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "babel",
+  coverageProvider: 'babel',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  coverageReporters: [
-    "json",
-    "text",
-    "lcov",
-    "clover"
-  ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
+import sum from '../sum';
 
 describe('A test', () => {
   it('2 + 2 = 4', () => {
-    expect(2 + 2).toEqual(4);
+    expect(sum(2, 2)).toEqual(4);
   });
 });
 
