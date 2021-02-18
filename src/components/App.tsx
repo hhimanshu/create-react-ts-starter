623dbbac715eed06db024ce9a2d5fee3c47f1140
Harit Himanshu <hhimanshu@Harits-MacBook-Pro.local>
2021-02-18 13:26:50 -0800

    Updated documentation and wordings on index.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
 const App = () => {
-  return <h1>Hello React in TypeScript + Jest + React Testing Library + Storybook</h1>;
+  return (
+    <h1>
+      Hello React in TypeScript + Jest + React Testing Library + Storybook
+    </h1>
+  );
 };
 export default App;
