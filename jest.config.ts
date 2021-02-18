2ce8fe980ea4810a414dbb8815cb957236103be3
hhimanshu <harit.himanshu@yahoo.in>
2021-02-17 17:44:57 -0800

    Added test dependencies of Jest and Enzyme

---
+++ jest.config.ts
@@ -0,0 +1,5 @@
+module.exports = {
+    "jest": {
+        setupTestFrameworkScriptFile: "<rootDir>/tests.config.ts",
+    }
+}
---
+++ src/components/App.test.tsx
@@ -0,0 +1,5 @@
+describe("A test", () => {
+    it('2 + 2 = 4', () => {
+        expect(2 + 2).toEqual(4)
+    });
+})
