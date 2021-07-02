473760363df8d02bff6ffa4030d072ce2158bf45
hhimanshu <harit.himanshu@yahoo.in>
2021-07-02 10:35:25 -0700

    feat(DEV-17): add react-router support
    failing test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
-const App = () => {
-  return (
-    <h1>
-      Hello React in TypeScript + Jest + React Testing Library + Storybook
-    </h1>
-  );
-};
+const App = () => (<Router>
+    <Switch>
+      <Route path='/' exact component={Home} />
+      <Route path='/page1' component={Page1} />
+      <Route path='/page2' component={Page2} />
+    </Switch>
+  </Router>
+);
 export default App;
